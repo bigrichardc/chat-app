78cb95619f9655eec6ae69f699bab2d359d2a60c
Richard Clifford <r.clifford@bham.ac.uk>
2020-01-13 18:32:49 +0000

    Added chats functionality, not quite working

--- src/components/chats/ChatContainer.js
+++ src/components/chats/ChatContainer.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import SideBar from './SideBar'
+import {COMMUNITY_CHAT, MESSAGE_SENT, MESSAGE_RECIEVED, TYPING} from '../../Events'
+import ChatHeading from './ChatHeading'
+import Messages from '../messages/Messages'
+import MessageInput from '../messages/MessageInput'
 
 export default class ChatContainer extends Component {
 
@@ -11,6 +15,59 @@ export default class ChatContainer extends Component {
             activeChat: null,
         }
     }
+
+    componentDidMount() {
+		const { socket } = this.props
+		socket.emit(COMMUNITY_CHAT, this.resetChat)
+	}
+
+    resetChat = (chat)=>{
+        return this.addChat(chat, true)
+    }
+
+	addChat = (chat, reset)=>{
+		const { socket } = this.props
+		const { chats } = this.state
+
+		const newChats = reset ? [chat] : [...chats, chat]
+		this.setState({chats:newChats, activeChat:reset ? chat : this.state.activeChat})
+
+		const messageEvent = `${MESSAGE_RECIEVED}-${chat.id}`
+		const typingEvent = `${TYPING}-${chat.id}`
+
+		socket.on(typingEvent, this.updateTypingInChat(chat.id))
+		socket.on(messageEvent, this.addMessageToChat(chat.id))
+	}
+
+    
+
+    addMessageToChat = (chatId)=>{
+        return message => {
+            const {chats} = this.state
+            let newChats = chats.map((chat)=>{
+                if (chat.id === chatId)
+                    chat.messages.push(message)
+                return chat
+                })
+
+                this.setState({chats:newChats})
+            }
+    }
+
+    updateTypingInChat = (chatId)=>{
+
+    }
+
+    sendMessage= (chatId, message)=> {
+        const {socket} = this.props
+        socket.emit(MESSAGE_SENT, {chatId, message})
+    }
+
+    sendTyping= (chatId, isTyping)=> {
+        const {socket} = this.props
+        socket.emit(TYPING, {chatId, isTyping})
+    }
+
 
     setActiveChat = (activeChat)=>{
         this.setState({activeChat})
@@ -27,6 +84,37 @@ export default class ChatContainer extends Component {
                 activeChat = {activeChat}
                 setActiveChat = {this.setActiveChat}
                 />
+                <div className="chat-room-container">
+					{
+						activeChat !== null ? (
+
+							<div className="chat-room">
+								<ChatHeading name={activeChat.name} />
+								<Messages 
+									messages={activeChat.messages}
+									user={user}
+									typingUsers={activeChat.typingUsers}
+									/>
+								<MessageInput 
+									sendMessage={
+										(message)=>{
+											this.sendMessage(activeChat.id, message)
+										}
+									}
+									sendTyping={
+										(isTyping)=>{
+											this.sendTyping(activeChat.id, isTyping)
+										}
+									}
+									/>
+
+							</div>
+						):
+						<div className="chat-room choose">
+							<h3>Choose a chat!</h3>
+						</div>
+					}
+				</div>
             </div>
         )
     }
